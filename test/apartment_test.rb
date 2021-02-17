dfa2efd35294d1d809b62d3505ce831b0e4f4710
ignored-comment <info.konham@gmail.com>
2021-02-17 11:17:02 -0500

    Complete iteration 1

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -4,4 +4,17 @@ require './lib/apartment'
 require './lib/renter'
 
 class ApartmentTest < Minitest::Test
+  def test_it_exists
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_instance_of Apartment, unit1
+  end
+
+  def test_it_can_add_renter
+    renter1 = Renter.new("Jessie")
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_nil nil, unit1.renter
+
+    unit1.add_renter(renter1)
+    assert_equal renter1, unit1.renter
+  end
 end
